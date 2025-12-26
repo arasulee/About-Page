b3ece950cb318acecdd060069b8d5ed9221efb54
arasu-murali <arasu.king.014@gmail.com>
2025-12-26 20:02:03 +0530

    Final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/About-Page',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
